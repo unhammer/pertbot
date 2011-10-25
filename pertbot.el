cd6ec2ef8107d55fdc07db05cf98513e7342b6a8
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2011-10-25 18:17:49 +0200

    ,hindi

--- pertbot.el
+++ pertbot.el
@@ -15,7 +15,10 @@
 (defun pertbot-handle-match (match-type nickuserhost msg)
   (cond ((eq match-type 'keyword)
 	 (cond ((equal ",iso" (substring-no-properties msg 0 4))
-		(pertbot-handle-iso msg))))
+		(pertbot-handle-iso msg))
+	       ;; TODO: database thingy from erbot
+	       ((equal ",hindi"  (substring-no-properties msg 0 6))
+		(erc-send-message "If you want to work on Hindi: the first thing you need to do is convert the IIIT analyser of Hindi http://wiki.apertium.org/wiki/Talk:Hindi_and_Urdu") )))
 	;; guess this matches even if its us saying something :-/
 	;; ((eq match-type 'current-nick) (erc-send-message "hi there"))
 	))
