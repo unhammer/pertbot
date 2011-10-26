d80c94b2c69a86d0ba8e4ea3ec6140454ea5cd19
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2011-10-26 10:33:15 +0200

    move pertbot-simple-answers into user file

--- example-pertbot-load.el
+++ example-pertbot-load.el
@@ -1,10 +1,14 @@
 ;; Change these values:
-(defvar erc-nick "pertbot")
-(defvar erc-password "somedifficultpassword")
-(defvar erc-server "irc.freenode.net")
+(setq erc-nick "pertbot")
+(setq erc-password "somedifficultpassword")
+(setq erc-server "irc.freenode.net")
 (add-to-list 'load-path "~/pertbot")	; path to this directory
 
 (setq erc-autojoin-channels-alist '(("freenode.net" "##test")))
+
+(setq pertbot-simple-answers
+  '(("hi" . "Yo!")
+    ("ciao" . "ciao")))
 
 ;; Leave these:
 (require 'pertbot)
--- pertbot.el
+++ pertbot.el
@@ -1,28 +1,49 @@
 (require 'iso-639)
 
-(setq erc-keywords '(",iso" ",hindi"))
+(setq erc-keywords '("iso" "help" "commands"))
+
+(defun pertbot-handle-help (msg)
+  (erc-send-message "I've just been born so I don't know many commands. Try ,commands for a full list."))
+
+(defun pertbot-handle-commands (msg)
+  (erc-send-message (format "Known commands: %s"
+			    (apply #'concat
+				   (mapcar (lambda (key)
+					     (format ",%s " key))
+					   erc-keywords)))))
 
 (defun pertbot-handle-iso (msg)
-  (if (string-match ",iso \\(...?\\)\n" msg)
+  (if (string-match "^\\Sw*,iso\\Sw+\\(...?\\)\\Sw*\n" msg)
       (let* ((code (match-string 1 msg))
 	     (lang (assoc code iso-639-language-codes))
 	     (reply (if lang
 			(format "ISO language code \"%s\" is %s" code (cdr lang))
 		      (format "Unknown ISO language code: %s" code))))
 	(erc-send-message reply))
-    (erc-send-message "Uhwhatnow?")))
+    (erc-send-message "Usage: ,iso CODE")
+    (erc-send-message "where CODE is a two- or three-letter ISO-639 language code.")))
 
 (defun pertbot-handle-match (match-type nickuserhost msg)
   (cond ((eq match-type 'keyword)
-	 (cond ((equal ",iso" (substring-no-properties msg 0 4))
-		(pertbot-handle-iso msg))
-	       ;; TODO: database thingy from erbot
-	       ((equal ",hindi"  (substring-no-properties msg 0 6))
-		(erc-send-message "If you want to work on Hindi: the first thing you need to do is convert the IIIT analyser of Hindi http://wiki.apertium.org/wiki/Talk:Hindi_and_Urdu"))))
+	 (string-match "^\\Sw*,\\Sw*\\(\\sw+\\)" msg)
+	 (let* ((key (downcase (match-string 1 msg)))
+		(simple-answer (assoc key pertbot-simple-answers)))
+	   (cond ((equal key "iso") (pertbot-handle-iso msg))
+		 ((equal key "help") (pertbot-handle-help msg))
+		 ((equal key "commands") (pertbot-handle-commands msg))
+		 (simple-answer
+		  (erc-send-message (cdr simple-answer))))))
 	;; guess this matches even if its us saying something :-/
 	;; ((eq match-type 'current-nick) (erc-send-message "hi there"))
 	))
 
+(defvar pertbot-simple-answers
+  '(("hi" . "Hello there"))
+  "Association list of questions and answers, simple strings (no functions). Meant to be overridden by the user")
+
+;; User should define pertbot-simple-answers before requiring this:
+(mapc (lambda (key) (add-to-list 'erc-keywords key))
+      (mapcar #'car pertbot-simple-answers))
 
 (add-hook 'erc-text-matched-hook 'pertbot-handle-match)
 
