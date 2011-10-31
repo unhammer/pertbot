3370bc38b1ecc39e836ac121b8a99b82e7ac75fc
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2011-10-31 15:44:42 +0100

    ensure we have a string-match before trying match-string (silences a warning); no spaces after comma; include comma in erc-keywords

--- pertbot.el
+++ pertbot.el
@@ -2,7 +2,7 @@
 (require 'pertbot-apertium-pairs)
 (require 'pertbot-translate)
 
-(setq erc-keywords '("iso" "help" "commands" "pairs" "translate" "installed" "follow" "unfollow"))
+(setq erc-keywords '(",iso" ",help" ",commands" ",pairs" ",translate" ",installed" ",follow" ",unfollow"))
 
 (defun pertbot-handle-help (msg)
   (erc-send-message "Try ,commands to see a full list of commands. The most useful are probably ,iso and ,pairs."))
@@ -11,7 +11,7 @@
   (erc-send-message (format "Known commands: %s"
 			    (apply #'concat
 				   (mapcar (lambda (key)
-					     (format ",%s " key))
+					     (format "%s " key))
 					   erc-keywords)))))
 
 (defun pertbot-handle-iso (msg)
@@ -27,26 +27,26 @@
 
 (defun pertbot-handle-match (match-type nickuserhost msg)
   (cond ((eq match-type 'keyword)
-	 (string-match "^\\Sw*,\\Sw*\\(\\sw+\\)" msg)
-	 (let* ((key (downcase (match-string-no-properties 1 msg)))
-		(simple-answer (assoc key pertbot-simple-answers)))
-	   (cond ((equal key "iso") (pertbot-handle-iso msg))
-		 ;; TODO: use the method in `pcomplete-erc-commands'
-		 ;; for auto-creating these functions with
-		 ;; `apropos-internal'
-		 ((equal key "help") (pertbot-handle-help msg))
-		 ((equal key "commands") (pertbot-handle-commands msg))
-		 ((equal key "pairs") (pertbot-handle-pairs msg))
-		 ((equal key "translate") (pertbot-handle-translate msg))
-		 ((equal key "installed") (pertbot-handle-installed msg))
-		 ((equal key "follow") (pertbot-handle-follow nickuserhost msg))
-		 ((equal key "unfollow") (pertbot-handle-unfollow nickuserhost msg))
-		 (simple-answer
-		  (erc-send-message (cdr simple-answer))))))
+	 (when (string-match "^\\Sw*,\\(\\sw+\\)" msg)
+	   (let* ((key (downcase (match-string-no-properties 1 msg)))
+		  (simple-answer (assoc key pertbot-simple-answers)))
+	     (cond ((equal key "iso") (pertbot-handle-iso msg))
+		   ;; TODO: use the method in `pcomplete-erc-commands'
+		   ;; for auto-creating these functions with
+		   ;; `apropos-internal'
+		   ((equal key "help") (pertbot-handle-help msg))
+		   ((equal key "commands") (pertbot-handle-commands msg))
+		   ((equal key "pairs") (pertbot-handle-pairs msg))
+		   ((equal key "translate") (pertbot-handle-translate msg))
+		   ((equal key "installed") (pertbot-handle-installed msg))
+		   ((equal key "follow") (pertbot-handle-follow nickuserhost msg))
+		   ((equal key "unfollow") (pertbot-handle-unfollow nickuserhost msg))
+		   (simple-answer
+		    (erc-send-message (cdr simple-answer)))))))
 	((eq match-type 'pal) (pertbot-handle-follow-match nickuserhost msg))
-	;; guess this matches even if its us saying something :-/
-	;; ((eq match-type 'current-nick) (erc-send-message "hi there"))
-	))
+	((and (eq match-type 'current-nick)
+	      (string-match (concat "^" erc-nick) msg))
+	 (erc-send-message "hi there"))))
 (add-hook 'erc-text-matched-hook 'pertbot-handle-match)
 
 
@@ -54,10 +54,12 @@
 
 (defvar pertbot-simple-answers
   '(("hi" . "Hello there"))
-  "Association list of questions and answers, simple strings (no functions). Meant to be overridden by the user")
+  "Association list of questions and answers, simple strings (no
+  functions). Meant to be overridden by the user. Unlike
+  `erc-keywords', no initial comma.")
 
 ;; User should define pertbot-simple-answers before requiring this:
-(mapc (lambda (key) (add-to-list 'erc-keywords key))
+(mapc (lambda (key) (add-to-list 'erc-keywords (concat "," key)))
       (mapcar #'car pertbot-simple-answers))
 
 (provide 'pertbot)
